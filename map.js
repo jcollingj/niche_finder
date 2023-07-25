723a96dffbe1a6e8f998451ed85749a93e7e6212
jcollingj <jcollingj@gmail.com>
2023-07-25 16:34:28 -0600

    Update map.js

--- map.js
+++ map.js
@@ -1,194 +1,180 @@
 function create_map(nodesParam, edgesParam, rootIdParam, formatted) {
-  const rootId = rootIdParam
-  let nodes = []
-  let edges = []
-  if (formatted) {
-    nodes = nodesParam
-    edges = edgesParam
-  } else {
-    const edgesSplit = edgesParam.split("|")
-    const nodesSplit = nodesParam.split("|")
-    for (let i = 0; i < edgesSplit.length; i++) {
-      let split2 = edgesSplit[i].split("+")
-      edges.push({
-        source: split2[0],
-        target: split2[1]
-      })
-    }
-    for (let i = 0; i < nodesSplit.length; i += 1) {
-      // If the node ID is undefined, skip the node
-      if (nodesSplit[i] === undefined) {
-        continue
-      }
-	    nodes.push({
-      	id: nodesSplit[i].trim()
-      })
-    }
-  }
-  
-  // Iterate backwards through the list of edges
-  for (var i = edges.length - 1; i >= 0; i--) {
-    // If the edge is a self-loop, remove it
-    if (edges[i].source === edges[i].target) {
-      edges.splice(i, 1);
-    }
-  }
-
-
-
-  for (let i = 0; i < nodes.length; i += 1) {
-    nodes[i]['collapsed'] = nodes[i].id !== rootId
-    nodes[i]['childLinks'] = []
-    nodes[i]['previousX'] = -1
-    nodes[i]['previousY'] = -1
-    nodes[i]['colorParam'] = ''
-  }
-  const gData = {
-    nodes: nodes,
-    links: edges
-  }
-  const nodesById = Object.fromEntries(gData.nodes.map(node => [node.id, node]));
-  gData.links.forEach(link => {
-    nodesById[link.source].childLinks.push(link);
-  });
-  const getPrunedTree = () => {
-    const visibleNodes = [];
-    const visibleLinks = [];
-    (function traverseTree(node = nodesById[rootId]) {
-      visibleNodes.push(node);
-      if (node.collapsed) return;
-      visibleLinks.push(...node.childLinks);
-      node.childLinks
-        .map(link => ((typeof link.target) === 'object') ? link.target : nodesById[link.target]) // get child node
-        .forEach(traverseTree);
-    })(); // IIFE
-
-    return { nodes: visibleNodes, links: visibleLinks };
-  };
-  function textSplitter(text) {
-    // initialize final array
-    var finalArray = [''];
-    // Split the text into an array
-    var textArray = text.split(" ");
-    // Loop through the array
-    let positionInFinalArray = 0
-    for (var i = 0; i < textArray.length; i++) {
-      // If the word is longer than 10 characters
-      const lenCurrentWord = textArray[i].length
-      const lenCurrentPositionInFinalArray = finalArray[positionInFinalArray].length
-      // If the length of the current position is under 16, considering adding the current word
-      if ((lenCurrentPositionInFinalArray + lenCurrentWord) < 15) {
-        // Add the word to the current position
-        finalArray[positionInFinalArray] += textArray[i] + " ";
-      } else {
-        // Add the word to the next position
-        positionInFinalArray += 1
-        finalArray[positionInFinalArray] = textArray[i] + " ";
-      }
-    }
-    for (let i = 0; i < finalArray.length; i += 1) {
-      finalArray[i] = finalArray[i].trim()
-    }
-    return finalArray;
-  }
-  function drawFunction({ id, x, y, childLinks, colorParam }, color, ctx) {
-    // const textList = id.match(/\b[\w']+(?:[^\w\n]+[\w']+){0,2}\b/g);
-    const textList = textSplitter(id)
-    var line_height = 5
-    const numChildren = childLinks.length;
-
-    // Draw the node. White fill and back ground
-    ctx.beginPath();
-    if (numChildren === 0) {
-      ctx.fillStyle = 'grey';
-    } else if (colorParam.length > 0) {
-      ctx.fillStyle = colorParam;
+    const rootId = rootIdParam
+    let nodes = []
+    let edges = []
+    if (formatted) {
+        nodes = nodesParam
+        edges = edgesParam
     } else {
-      ctx.fillStyle = 'white';
+        const edgesSplit = edgesParam.split("|")
+        const nodesSplit = nodesParam.split("|")
+        for (let i = 0; i < edgesSplit.length; i++) {
+            let split2 = edgesSplit[i].split("+")
+            edges.push({
+                source: split2[0],
+                target: split2[1]
+            })
+        }
+        for (let i = 0; i < nodesSplit.length; i += 1) {
+            // If the node ID is undefined, skip the node
+            if (nodesSplit[i] === undefined) {
+                continue
+            }
+            nodes.push({
+                id: nodesSplit[i].trim()
+            })
+        }
     }
 
-    ctx.arc(x, y, 17, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-
-
-
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.font = '4px Arial';
-    ctx.fillStyle = 'black';
-    const numLines = textList.length
-    for (let i = 0; i < textList.length; i += 1) {
-      if (numLines > 1) {
-        ctx.fillText(textList[i], x, (y + (i * line_height) - (numLines / 3 * line_height)));
-      } else {
-        ctx.fillText(textList[i], x, y);
-      }
-
+    // Iterate backwards through the list of edges
+    for (var i = edges.length - 1; i >= 0; i--) {
+        // If the edge is a self-loop, remove it
+        if (edges[i].source === edges[i].target) {
+            edges.splice(i, 1);
+        }
     }
 
 
 
-
-
-  }
-  const elem = document.getElementById('graph');
-  const Graph = ForceGraph()(elem)
-    .graphData(getPrunedTree())
-    .nodeCanvasObject((node, ctx) => drawFunction(node, 'color', ctx))
-    .onNodeHover(node => elem.style.cursor = node && node.childLinks.length ? 'pointer' : null)
-    .height(window.innerHeight - 60)
-  // Call back function that prints the event
-  // .linkDirectionalParticles(1)
-  // .linkDirectionalParticleWidth(2.5)
-  // .nodeColor(node => !node.childLinks.length ? 'green' : node.collapsed ? 'red' : 'yellow');
-  // This works but is horribly not smooth
-  // Graph.d3Force('collide', d3.forceCollide(30))
-  
-  Graph.d3Force('charge', d3.forceManyBody().strength(-300))
-  // Create a new link force that shortens the link distance
-  // const linkForce = d3.forceLink().distance(100)
-  // Add the link force to the graph
-  // Graph.d3Force('link', linkForce)
-  Graph.onNodeClick((node) => {
-    if (node.childLinks.length) {
-      node.collapsed = !node.collapsed; // toggle collapse state
-      if (node.collapsed === false) {
-        node.fx = null
-        node.fy = null
-        node.previousX = node.x
-        node.previousY = node.y
-        node.colorParam = '#f2f2f2'
-      } else if (node.collapsed === true && node.previousX !== -1 && node.previousY !== -1) {
-        // node.x = node.previousX
-        // node.y = node.previousY
-        node.fx = node.previousX
-        node.fy = node.previousY
-        node.colorParam = '#f2f2f2'
-      }
-      Graph.graphData(getPrunedTree());
-
-      // Wait half a second and then get the new x/y coordinates
-      setTimeout(() => {
-        Graph.centerAt(node.x, node.y, 500)
-        node.fx = null
-        node.fy = null
-
-      }, 500)
+    for (let i = 0; i < nodes.length; i += 1) {
+        nodes[i]['collapsed'] = nodes[i].id !== rootId
+        nodes[i]['childLinks'] = []
+        nodes[i]['previousX'] = -1
+        nodes[i]['previousY'] = -1
+        nodes[i]['colorParam'] = ''
     }
-  })
+    const gData = {
+        nodes: nodes,
+        links: edges
+    }
+    const nodesById = Object.fromEntries(gData.nodes.map(node => [node.id, node]));
+    gData.links.forEach(link => {
+        nodesById[link.source].childLinks.push(link);
+    });
+    const getPrunedTree = () => {
+        const visibleNodes = [];
+        const visibleLinks = [];
+        (function traverseTree(node = nodesById[rootId]) {
+            visibleNodes.push(node);
+            if (node.collapsed) return;
+            visibleLinks.push(...node.childLinks);
+            node.childLinks
+                .map(link => ((typeof link.target) === 'object') ? link.target : nodesById[link.target]) // get child node
+                .forEach(traverseTree);
+        })(); // IIFE
 
-  // Get the center of the div with id "graph"
-  // const graphDiv = document.getElementById('graph')
-  // const graphDivWidth = graphDiv.offsetWidth
-  // const graphDivHeight = graphDiv.offsetHeight
-  // const graphDivCenterX = graphDivWidth / 2
-  // const graphDivCenterY = graphDivHeight / 2
-  // Get the center of the graph
-  // Graph.centerAt(graphDivCenterX, graphDivCenterY, 0)
-  elementResizeDetectorMaker().listenTo(
-    document.getElementById('graph'),
-    el => Graph.width(el.offsetWidth)
-  );
-  setTimeout(() => Graph.zoomToFit(400, 125), 500);
+        return { nodes: visibleNodes, links: visibleLinks };
+    };
+    function textSplitter(text) {
+        // initialize final array
+        var finalArray = [''];
+        // Split the text into an array
+        var textArray = text.split(" ");
+        // Loop through the array
+        let positionInFinalArray = 0
+        for (var i = 0; i < textArray.length; i++) {
+            // If the word is longer than 10 characters
+            const lenCurrentWord = textArray[i].length
+            const lenCurrentPositionInFinalArray = finalArray[positionInFinalArray].length
+            // If the length of the current position is under 16, considering adding the current word
+            if ((lenCurrentPositionInFinalArray + lenCurrentWord) < 15) {
+                // Add the word to the current position
+                finalArray[positionInFinalArray] += textArray[i] + " ";
+            } else {
+                // Add the word to the next position
+                positionInFinalArray += 1
+                finalArray[positionInFinalArray] = textArray[i] + " ";
+            }
+        }
+        for (let i = 0; i < finalArray.length; i += 1) {
+            finalArray[i] = finalArray[i].trim()
+        }
+        return finalArray;
+    }
+    function drawFunction({ id, x, y, childLinks, colorParam }, color, ctx) {
+        // const textList = id.match(/\b[\w']+(?:[^\w\n]+[\w']+){0,2}\b/g);
+        const textList = textSplitter(id)
+        var line_height = 5
+        const numChildren = childLinks.length;
+
+        // Draw the node. White fill and back ground
+        ctx.beginPath();
+        if (numChildren === 0) {
+            ctx.fillStyle = 'grey';
+        } else if (colorParam.length > 0) {
+            ctx.fillStyle = colorParam;
+        } else {
+            ctx.fillStyle = 'white';
+        }
+
+        ctx.arc(x, y, 17, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+
+
+
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.font = '4px Arial';
+        ctx.fillStyle = 'black';
+        const numLines = textList.length
+        for (let i = 0; i < textList.length; i += 1) {
+            if (numLines > 1) {
+                ctx.fillText(textList[i], x, (y + (i * line_height) - (numLines / 3 * line_height)));
+            } else {
+                ctx.fillText(textList[i], x, y);
+            }
+
+        }
+
+
+
+
+
+    }
+    const elem = document.getElementById('graph');
+    const Graph = ForceGraph()(elem)
+        .height('500')
+        .graphData(getPrunedTree())
+        .nodeCanvasObject((node, ctx) => drawFunction(node, 'color', ctx))
+        .onNodeHover(node => elem.style.cursor = node && node.childLinks.length ? 'pointer' : null)
+
+    Graph.d3Force('charge', d3.forceManyBody().strength(-300))
+    // Create a new link force that shortens the link distance
+    // const linkForce = d3.forceLink().distance(100)
+    // Add the link force to the graph
+    // Graph.d3Force('link', linkForce)
+    Graph.onNodeClick((node) => {
+        if (node.childLinks.length) {
+            node.collapsed = !node.collapsed; // toggle collapse state
+            if (node.collapsed === false) {
+                node.fx = null
+                node.fy = null
+                node.previousX = node.x
+                node.previousY = node.y
+                node.colorParam = '#f2f2f2'
+            } else if (node.collapsed === true && node.previousX !== -1 && node.previousY !== -1) {
+                // node.x = node.previousX
+                // node.y = node.previousY
+                node.fx = node.previousX
+                node.fy = node.previousY
+                node.colorParam = '#f2f2f2'
+            }
+            Graph.graphData(getPrunedTree());
+
+            // Wait half a second and then get the new x/y coordinates
+            setTimeout(() => {
+                Graph.centerAt(node.x, node.y, 500)
+                node.fx = null
+                node.fy = null
+
+            }, 500)
+        }
+    })
+
+    elementResizeDetectorMaker().listenTo(
+        document.getElementById('graph'),
+        el => Graph.width(el.offsetWidth)
+    );
+    setTimeout(() => Graph.zoomToFit(400, 60), 500);
 }
